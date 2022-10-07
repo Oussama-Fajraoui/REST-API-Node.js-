90df5198dd6103cf3d6aed5d3e609ef1b01b6b8b
Oussama-Fajraoui <oussama.fajraoui1@gmail.com>
2022-10-07 12:56:00 +0100

    GET

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const http = require('http');
 // require("dotenv").config();
+const GetReq = require('./routes/GetRequet');
+
+let movies = require('./api/movies.json');
+
 
 const PORT = process.env.PORT || 5001;
 
 // Creating HTTP server :
 
 const server = http.createServer((req,res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', "application/json")
-    res.write(JSON.stringify({message: "Hello world"}))
-    res.end()
+    req.movies = movies
+    switch (req.method) {
+        case "GET":
+            GetReq(req, res);
+            break;
+            default: 
+            res.statusCode = 404;
+            res.setHeader("Content-Type", "application/json");
+            res.write(
+                JSON.stringify({ title: "Not Found", message: "Route not Found" })
+            );
+            res.end();
+    }
 }) ;
 
 server.listen(PORT, () => {
